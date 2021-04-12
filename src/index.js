c30209773b8b23ead3ed86ee770b835c158baa97
jy411 <jinyungisnot@gmail.com>
2021-04-12 10:26:47 +0800

    almost there

--- src/index.js
+++ src/index.js
@@ -23,10 +23,8 @@ async function startApolloServer(port, callback) {
         res.end();
     });
 
-    await new Promise((resolve) => app.listen({ port: 4000 }, resolve));
-    // console.log(
-    //     `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
-    // );
+    await new Promise((resolve) => app.listen({ port: process.env.PORT }, resolve));
+
     return { server, app };
 }
 
