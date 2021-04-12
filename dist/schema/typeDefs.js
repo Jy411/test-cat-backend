4693ef4e560162396683d463cbc27228ceae97d6
jy411 <jinyungisnot@gmail.com>
2021-04-12 14:29:51 +0800

    updated scripts

---
+++ dist/schema/typeDefs.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const {
+  gql
+} = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        fetchCats: [Cat]!
+    }
+
+    type Mutation {
+        unlikeCat: CatResponse!
+        likeCat: CatResponse!
+    }
+
+    type Cat {
+        id: ID!
+        name: String!
+    }
+
+    type CatResponse {
+        success: Boolean!
+        error: String
+        data: Cat
+    }
+`;
+module.exports = typeDefs;
